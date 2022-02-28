71fdb0b3bd72e3ed50348a39e6adf5cbaf99dd2b
Ivo Yankov <ivo@devlabs.bg>
2022-02-28 16:07:17 +0200

    add `update-version` script

---
+++ update-version.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+try {
+    const data = fs.readFileSync(path.resolve(__dirname, 'version.txt'), 'utf8')
+    console.log(data);
+    let version = parseInt(data.trim());
+    version++;
+    fs.writeFileSync( path.resolve(__dirname, 'version.txt'), version );
+} catch (err) {
+    console.error(err)
+}
