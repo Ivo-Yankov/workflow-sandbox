60574056a11e1d1d5c96163eb9a71ab08fda16d7
Ivo Yankov <ivo@devlabs.bg>
2022-03-01 17:37:19 +0200

    test88

--- update-version.js
+++ update-version.js
@@ -2,14 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 try {
-
-    console.log(process.argv);
     let data = fs.readFileSync(path.resolve(__dirname, 'version.txt'), 'utf8')
-    console.log(data);
-    let versions = data.split['.'];
+    let versions = data.split('.');
     let patch = parseInt(versions[2]);
     patch++;
     let version = `0.0.${patch}`;
+
+    console.log(version);
 
     fs.writeFileSync( path.resolve(__dirname, 'version.txt'), version );
 } catch (err) {
