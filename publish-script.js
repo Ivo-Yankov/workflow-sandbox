b8fbb63003f9f64aa34952aa11654eb4f1a9cde5
Ivo Yankov <ivo@devlabs.bg>
2022-03-01 14:21:48 +0200

    test88

--- publish-script.js
+++ publish-script.js
@@ -2,13 +2,15 @@ const {publish} = require("libnpmpublish");
 const pacote = require('pacote') // see: http://npm.im/pacote
 const path = require('path');
 
-const packagePath = path.resolve(__dirname)
-
-const manifest = await pacote.manifest(packagePath)
-const tarData = await pacote.tarball(packagePath)
 
 
 (async function () {
+
+    const packagePath = path.resolve(__dirname)
+
+    const manifest = await pacote.manifest(packagePath)
+    const tarData = await pacote.tarball(packagePath)
+
     console.log("Publishing Wooo Hooo");
     console.log(process.env);
     console.log(process.argv);
