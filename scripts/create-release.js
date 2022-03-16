250891d0155f9ef8c0e74cfcd534e6cf86d4d583
Ivo Yankov <ivo@devlabs.bg>
2022-03-16 15:46:56 +0200

    test88

--- scripts/create-release.js
+++ scripts/create-release.js
@@ -1,12 +1,14 @@
 const {getLatestRelease, deleteRelease, createRelease} = require('./helpers');
 
-(async function() {
+(async () => {
     try {
         let latestRelease = await getLatestRelease();
         if (latestRelease && latestRelease.prerelease) {
             if (latestRelease && latestRelease.prerelease) {
                 await deleteRelease(latestRelease.id.toString());
             }
+
+            latestRelease.name = "This is the new name";
 
             await createRelease(latestRelease);
         }
