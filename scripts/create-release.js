6a8563b119e445d0a4a5ad33320bc2307a1bc18d
Ivo Yankov <ivo@devlabs.bg>
2022-03-17 09:09:59 +0200

    test88

--- scripts/create-release.js
+++ scripts/create-release.js
@@ -1,3 +1,4 @@
+const {exec} = require('child_process');
 const {getLatestRelease, deleteRelease, createRelease} = require('./helpers');
 
 const sleep = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
@@ -8,6 +9,15 @@ const sleep = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
         if (latestRelease && latestRelease.prerelease) {
             if (latestRelease && latestRelease.prerelease) {
                 await deleteRelease(latestRelease.id.toString());
+
+                const {tag_name} = latestRelease;
+
+                // Delete the tag:
+                await exec(`git push --delete origin ${tag_name}`);
+
+                // Recreate the tag:
+                await exec(`git tag ${tag_name}`);
+                await exec(`git push origin --tags`);
             }
 
             latestRelease.name = latestRelease.name
@@ -15,7 +25,7 @@ const sleep = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
                 .replace('major', '')
                 .trim();
 
-            await sleep(5000);
+            // await sleep(5000);
             await createRelease(latestRelease);
 
         }
